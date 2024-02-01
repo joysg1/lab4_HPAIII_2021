80a49e0a9a71a85f6fc3ad40ac9d81585a8ee4f1
joysg1 <joy.nelaton@utp.ac.pa>
2024-02-01 10:32:46 -0500

    Commit_01

---
+++ Joy_Nelaton_Lab4/Joy_Nelaton_Lab4/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnSiguiente = New System.Windows.Forms.Button()
+        Me.tbCantidad = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lbNotasFinales = New System.Windows.Forms.ListBox()
+        Me.lbEstudiantes = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(176, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(433, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "REGISTRO DE ESTUDIANTES"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnSiguiente)
+        Me.GroupBox1.Controls.Add(Me.tbCantidad)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(29, 91)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(271, 289)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "INGRESO DE INFORMACION"
+        '
+        'btnSiguiente
+        '
+        Me.btnSiguiente.Location = New System.Drawing.Point(49, 118)
+        Me.btnSiguiente.Name = "btnSiguiente"
+        Me.btnSiguiente.Size = New System.Drawing.Size(123, 36)
+        Me.btnSiguiente.TabIndex = 2
+        Me.btnSiguiente.Text = "SIGUIENTE"
+        Me.btnSiguiente.UseVisualStyleBackColor = True
+        '
+        'tbCantidad
+        '
+        Me.tbCantidad.Location = New System.Drawing.Point(153, 46)
+        Me.tbCantidad.Name = "tbCantidad"
+        Me.tbCantidad.Size = New System.Drawing.Size(48, 22)
+        Me.tbCantidad.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "# de Estudiantes: "
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lbNotasFinales)
+        Me.GroupBox2.Controls.Add(Me.lbEstudiantes)
+        Me.GroupBox2.Location = New System.Drawing.Point(328, 101)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(488, 279)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "RESULTADOS"
+        '
+        'lbNotasFinales
+        '
+        Me.lbNotasFinales.FormattingEnabled = True
+        Me.lbNotasFinales.ItemHeight = 16
+        Me.lbNotasFinales.Location = New System.Drawing.Point(284, 68)
+        Me.lbNotasFinales.Name = "lbNotasFinales"
+        Me.lbNotasFinales.Size = New System.Drawing.Size(171, 196)
+        Me.lbNotasFinales.TabIndex = 1
+        '
+        'lbEstudiantes
+        '
+        Me.lbEstudiantes.FormattingEnabled = True
+        Me.lbEstudiantes.ItemHeight = 16
+        Me.lbEstudiantes.Location = New System.Drawing.Point(20, 68)
+        Me.lbEstudiantes.Name = "lbEstudiantes"
+        Me.lbEstudiantes.Size = New System.Drawing.Size(171, 196)
+        Me.lbEstudiantes.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(850, 392)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "LAB 4 NELATON JOY"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnSiguiente As Button
+    Friend WithEvents tbCantidad As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents lbNotasFinales As ListBox
+    Friend WithEvents lbEstudiantes As ListBox
+End Class
---
+++ Joy_Nelaton_Lab4/Joy_Nelaton_Lab4/Form1.vb
@@ -0,0 +1,122 @@
+﻿Public Class Form1
+    Dim cantidad As Integer
+    Dim nombres() As String
+    Dim parciales() As Integer
+    Dim proyectos() As Integer
+    Dim semestrales() As Integer
+    Dim promedioFinal() As Integer
+    Dim notaFinal() As Char
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GroupBox2.Enabled = False
+    End Sub
+
+
+    Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        Verificar()
+    End Sub
+
+    Sub Verificar()
+        cantidad = Val(tbCantidad.Text)
+        If (cantidad <= 0) Then
+            MsgBox("Favor verifique el dato cantidad")
+        Else
+            Capturar_Datos()
+            Establecer_NotaFinal()
+            GroupBox2.Enabled = True
+            Resultados()
+        End If
+
+    End Sub
+
+    Sub Capturar_Datos()
+        ReDim nombres(cantidad)
+        ReDim parciales(cantidad)
+        ReDim proyectos(cantidad)
+        ReDim semestrales(cantidad)
+        ReDim promedioFinal(cantidad)
+        ReDim notaFinal(cantidad)
+
+        For i = 1 To cantidad
+            MsgBox("Estudiante #" & i)
+            nombres(i) = InputBox("Ingrese el nombre:")
+            parciales(i) = Val(InputBox("Ingrese el promedio obtenido de parciales:"))
+            proyectos(i) = Val(InputBox("Ingrese el promedio obtenido de proyectos:"))
+            semestrales(i) = Val(InputBox("Ingrese el promedio obtenido del semestral:"))
+            promedioFinal(i) = parciales(i) + proyectos(i) + semestrales(i)
+        Next
+
+
+
+
+
+    End Sub
+
+    Sub Establecer_NotaFinal()
+        For i = 0 To cantidad
+            Select Case promedioFinal(i)
+                Case 0 To 60
+                    notaFinal(i) = "F"
+
+                Case 61 To 70
+                    notaFinal(i) = "D"
+
+                Case 71 To 80
+                    notaFinal(i) = "C"
+
+                Case 81 To 90
+                    notaFinal(i) = "B"
+
+                Case Else
+                    notaFinal(i) = "A"
+
+            End Select
+        Next
+
+    End Sub
+
+    Sub Resultados()
+
+        Dim caaux As Integer = promedioFinal.Length - 1
+
+        Dim indice1(caaux) As Integer
+
+        For i = 0 To caaux
+            indice1(i) = i
+        Next
+
+        Array.Sort(promedioFinal, indice1)
+
+        Dim tempNombres(caaux) As String
+        Dim tempNotasFinales(caaux) As Char
+
+        Array.Clear(tempNombres, 0, tempNombres.Length)
+        Array.Clear(tempNotasFinales, 0, tempNombres.Length)
+
+        Dim pos As Integer
+
+        For i = 0 To caaux
+            pos = indice1(i)
+
+            tempNombres(i) = nombres(pos)
+
+            tempNotasFinales(i) = notaFinal(pos)
+
+        Next
+
+        Array.Copy(tempNombres, nombres, nombres.Length)
+        Array.Copy(tempNotasFinales, notaFinal, notaFinal.Length)
+
+
+
+        Dim r As Integer = 1
+
+        For i = cantidad To r Step -1     'Imprimimos en orden inverso, descendente
+
+            lbEstudiantes.Items.Add(nombres(i))
+            lbNotasFinales.Items.Add(notaFinal(i))
+        Next
+    End Sub
+
+
+End Class
